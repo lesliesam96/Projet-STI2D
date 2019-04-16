93972a5b15230dcdba4aff2ced8a37f2e6f46227
AfroMangue <46705790+AfroMangue@users.noreply.github.com>
2019-04-16 11:00:57 +0200

    Add files via upload

--- Ralph/New_code_poulailler.ino
+++ Ralph/New_code_poulailler.ino
@@ -1,82 +1,52 @@
-/*
- SMS sender
-
- This sketch, for the Arduino GSM shield,sends an SMS message
- you enter in the serial monitor. Connect your Arduino with the
- GSM shield and SIM card, open the serial monitor, and wait for
- the "READY" message to appear in the monitor. Next, type a
- message to send and press "return". Make sure the serial
- monitor is set to send a newline when you press return.
-
- Circuit:
- * GSM shield
- * SIM card that can send SMS
-
- created 25 Feb 2012
- by Tom Igoe
-
- This example is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/GSMExamplesSendSMS
-
- */
-
-// Include the GSM library
 #include <GSM.h>
-
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
 #define PINNUMBER "264843"
 
 // initialize the library instance
 GSM gsmAccess;
 GSM_SMS sms;
+char txtMsg[200] = "Le taux de luminosité du poulailler est inférieur à 40 lux.";
+char remoteNum[20] = "0769621300";
 
-// Definition des broches du capteur
-int "GND"
-int "ADDR"
-int "SCL"
-int "SDA"
-int "VCC"
+float sensorVoltage;
+float sensorValue;
+float UVIndex;
 
-void setup() {
-  // initialize serial communications and wait for port to open:
- pinMode(GND, INPUT);
- pinMode(ADDR, INPUT);
-  pinMode(SCL, INPUT);
-   pinMode(SDA, INPUT);
-    pinMode(VCC,INPUT);
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(0x29);
+
+
+void setup()
+
+{
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
-  Serial.println("SMS Messages Sender");
-
-  // connection state
   boolean notConnected = true;
 
-  // Start GSM shield
-  // If your SIM has PIN, pass it as a parameter of begin() in quotes
   while (notConnected) {
     if (gsmAccess.begin(PINNUMBER) == GSM_READY) {
       notConnected = false;
     } else {
       Serial.println("Not connected");
       delay(1000);
+
+
     }
   }
-
-  Serial.println("GSM initialized");
 }
-void loop(void) 
-{  
-  /* Get a new sensor event */ 
+
+
+void loop(void)
+{
+  /* Get a new sensor event */
   sensors_event_t event;
   tsl.getEvent(&event);
- 
+
   /* Display the results (light is measured in lux) */
   if (event.light)
   {
-    Serial.print("sensor.reading = ")
+    Serial.print("sensor.reading = ");
     Serial.print(event.light); Serial.println(" lux");
   }
   else
@@ -84,30 +54,26 @@ void loop(void)
     /* If event.light = 0 lux the sensor is probably saturated
        and no reliable data could be generated! */
     Serial.println("Sensor overload");
-     if (sensorValue <= 50) }
-  }
-  delay(250);
-}
-  // sms text
-  Serial.print("Now, enter SMS content: ");
-  char txtMsg[200];
-  readSerial(txtMsg);
-  Serial.println("SENDING");
-  Serial.println();
-  Serial.println("Message:");
-  Serial.println(txtMsg);
+    if (sensorValue <= 40) {
+      // send the message
+      Serial.println("Le message va etre envoyé");
+      sms.beginSMS("0769621300");
+      sms.print("Alerte, Problème de luminosité dans le poulailler");
+      sms.endSMS();
+      Serial.println("\nCOMPLETE!\n");
 
-  // send the message
-  Serial.println("Le message va etre envoyé");
-  sms.beginSMS("0769621300");
-  sms.print("Alerte, Problème de luminosité dans le poulailler");
-  sms.endSMS();
-  Serial.println("\nCOMPLETE!\n");
+
+    }
+  }
+
+  delay(250);
+
+
 }
 
 /*
   Read input serial
- */
+*/
 int readSerial(char result[]) {
   int i = 0;
   while (1) {
